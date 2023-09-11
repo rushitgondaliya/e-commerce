61acb609864f8a2f5911726e60da7901337eb556
rushitgondaliya <133621234+rushitgondaliya@users.noreply.github.com>
2023-09-11 22:57:24 +0530

    e-commerce

---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+module.exports.userService=require("./user.service");
+module.exports.categoryService=require("./category.service");
+module.exports.productService=require("./product.service");
+module.exports.orderService=require("./order.service");
+module.exports.cartService=require("./cart.service");
+module.exports.subCategoryService=require("./subCategory.service");
---
+++ src/validations/index.js
@@ -0,0 +1,6 @@
+module.exports.userValidation = require("./user.validation");
+module.exports.categoryValidation =require("./category.validation");
+module.exports.productValidation =require("./product.validation");
+module.exports.orderValidation=require("./order.validation");
+module.exports.cartValidation=require("./cart.validation");
+module.exports.subCategoryValidation=require("./subCategory.validation");
